e1360f5e77e2e4102fdc00def80482796ab3a100
Yvonne Kwarteng <yvonnekcnwl@yahoo.co.uk>
2024-11-13 13:09:31 +0000

    fix ci error for not finding attribute in file

--- etl_sanity_checks.py
+++ etl_sanity_checks.py
@@ -18,13 +18,13 @@ def test_dat_completeness():
 # Test if deptno is a manadatory column
 def test_deptno_for_null_value_check():
     target_df = pd.read_csv("employee_dept_no_copy_tgt.csv")
-    isDeptNoNull = target_df['deptno'].isnull().any()
+    isDeptNoNull = target_df['dept_no'].isnull().any()
     assert isDeptNoNull == False, "Department no is contains a null value - Please check"
 
 
 # Test if eno is always a primary key
 def test_eno_for_unique_value_check():
     target_df = pd.read_csv("employee_dept_no_copy_tgt.csv")
-    totalCount = target_df["eno"].count()
-    deptNoUniqueValueCount = len(target_df["eno"].unique())
+    totalCount = target_df["Eno"].count()
+    deptNoUniqueValueCount = len(target_df["Eno"].unique())
     assert totalCount == deptNoUniqueValueCount, "eno column values are not unique - please check"
