11a43e10082378e9af55428214df9c3be77c1018
Yvonne Kwarteng <yvonnekcnwl@yahoo.co.uk>
2024-11-13 10:43:03 +0000

    first commit

---
+++ check_duplicates.py
@@ -0,0 +1,51 @@
+import pandas as pd
+source = pd.read_csv("/Users/yvonnek/anaconda3/pkgs/data/employee_sal.csv")
+print("Test Case 1: Following are the column names in the source file : \n")
+print(source.columns)
+print("\n")
+print("Test Case 2: Rows X columns in the source file \n")
+print(source.shape)
+print("\n")
+print("Test Case 3: No. of rows under each columns \n")
+print(source.count())
+print("\n")
+
+print("Test Case 4: Duplicate records in the source \n")
+print(source.duplicated().sum())
+print("\n")
+
+print("Test Case 5: Duplicate records saved in the file below \n")
+dupes = source[source.duplicated()].to_csv("/Users/yvonnek/anaconda3/pkgs/data/employee_dupli2.csv")
+print("\n")
+
+print("Test Case 6: Check if NULL value exist in dept_name column \n")
+print(source[source['dept_name'].isnull()])
+print("\n")
+
+print("Test Case 6a: Check if NULL value exist in salary column \n")
+print(source[source['salary'].isnull()])
+print("\n")
+
+print("Test Case 7: Unique value of Eno column in the source \n")
+print(source['Eno'].unique())
+print("\n")
+
+print("Test Case 8: Unique value of emp_name column in the source \n")
+print(source['emp_name'].unique())
+print("\n")
+
+print("Test Case 9: Unique value of dept_name column in the source \n")
+print(source['dept_name'].unique())
+print("\n")
+
+print("Test Case 10: Unique value of salary column in the source \n")
+print(source['salary'].unique())
+print("\n")
+
+print("Test Case 11: Sample (top 5) records from source file \n")
+print(source.head())
+print("\n")
+
+print("Test Case 12: Sample (bottom 5) records from source file \n")
+print(source.tail())
+print("Test Completed...... \n")
---
+++ data_completeness.py
@@ -0,0 +1,8 @@
+import pymysql
+from pymysql import connect
+
+from sqlalchemy import create_engine
+import pandas as pd
+#engine = create_engine('mysql+pymsql://root:root%Mypassword1234@localhost:3308/empl_target')
+#connection = engine.connect()
+
